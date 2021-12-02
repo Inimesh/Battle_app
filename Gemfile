f4e205257d58b642855ddd79f96f0aa3c80de841
Inigo Meshoulam <inigomeshoulam@Inigos-MacBook-Air.local>
2021-12-02 16:08:51 +0000

    practice files from before deleted. Clean slate to start Battle app challenge.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'rack'
+gem 'webrick'
+
+gem 'rspec', :group => [:development, :test]
---
+++ app.rb
@@ -0,0 +1,15 @@
+require "sinatra"
+require "sinatra/reloader"
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  # our routes go here
+  get '/' do
+    "Hello Battle!"
+  end 
+  # Start the server if this file is executed directly
+  run! if app_file == $0
+end
