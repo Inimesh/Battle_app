91c09c8af04c01a98a7549731ba28b945936aea5
Inimesh <ijmeshoulam@gmail.com>
2021-12-03 11:12:37 +0000

    Added functionality: Players enter names and taken to play page which displays their names

--- app.rb
+++ app.rb
@@ -8,8 +8,15 @@ class Battle < Sinatra::Base
 
   # our routes go here
   get '/' do
-    "Testing infrastructure working!"
+    erb :index
   end 
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
   # Start the server if this file is executed directly
   run! if app_file == $0
 end
---
+++ spec/features/name_submission_spec.rb
@@ -0,0 +1,13 @@
+feature 'player name submission' do
+  scenario 'players can enter names and see them on screen' do
+    visit('/')
+    fill_in "player_1_name", with: "Ben"
+    fill_in "player_2_name", with: "Tom"
+    
+    click_button "Fight!"
+
+    expect(page).to have_content "Ben vs. Tom"
+
+
+  end
+end
