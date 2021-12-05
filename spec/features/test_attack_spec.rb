65074eac9d81ae0ebff85f78bd07c932b86ed586
Inimesh <ijmeshoulam@gmail.com>
2021-12-05 13:03:59 +0000

    improving tests

--- spec/features/test_attack_spec.rb
+++ spec/features/test_attack_spec.rb
@@ -4,4 +4,10 @@ feature 'Attack player' do
     click_on 'Tom'
     expect(page).to have_content 'Tom: 40 HP'
   end
+
+  scenario 'Player 2 attacks Player 1' do 
+    sign_in_and_play
+    click_on 'Ben'
+    expect(page).to have_content 'Ben: 40 HP'
+  end
 end
